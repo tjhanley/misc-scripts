3e02ad32b37b39b07de61a72b1d15320e1c123ff
Thomas Hanley <tomha@wepay.com>
2016-04-28 15:02:07 -0700

    Update brew list

--- brew-me.sh
+++ brew-me.sh
@@ -1,19 +1,15 @@
 #!/usr/bin/env bash
 # what to brew
 
+brew update
 brew install git
 brew install macvim --env-std --with-override-system-vim
 brew cask install atom
 brew install jq
 brew install gpg
-brew install arc
-brew install arcanist
-brew install homebrew/php/arcanist
-brew install ansible
 brew install pyenv
 brew install wget
 brew install tree
-brew install vmrun
 brew install openssl
 brew link --force openssl
 brew install z
