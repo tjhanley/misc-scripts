a0f653bf72c240414bd159c8af656c2f84db36c3
Thomas Hanley <tjhanleyx@gmail.com>
2016-04-28 14:52:39 -0700

    Create brew-me.sh

---
+++ brew-me.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# what to brew
+
+brew install git
+brew install macvim --env-std --with-override-system-vim
+brew cask install atom
+brew install jq
+brew install gpg
+brew install arc
+brew install arcanist
+brew install homebrew/php/arcanist
+brew install ansible
+brew install pyenv
+brew install wget
+brew install tree
+brew install vmrun
+brew install openssl
+brew link --force openssl
+brew install z
